022fd8033adb2fa536df3cf89e2b45e86f4fa8dd
daniel-baggerman <daniel@baggerman.org>
2020-01-18 20:21:01 -0500

    .

--- src/app/manage-election/manage-election.component.ts
+++ src/app/manage-election/manage-election.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
+import { ManageElectionService } from './manage-election.service';
 
 @Component({
   selector: 'app-manage-election',
@@ -9,19 +10,25 @@ import { Params, ActivatedRoute } from '@angular/router';
 export class ManageElectionComponent implements OnInit {
   show_election_details = false;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private election_manager: ManageElectionService) { }
 
   ngOnInit() {
+    // get the election information passed when coming from search_elections and store it.
+    let ls_state = history.state;
+    if(ls_state == null){
+      // if state null then have to retrieve necessary information from the database based on url_election_name. This would happen if the user navigated here by typing the URL into their browser's navigation bar.
+    }
+
+    let url_election_name = '';
     this.route.params.subscribe(
       (params: Params) => {
-        if (params['election_id']){
-          this.election_selected(params['election_id']);
-        } else {
-          console.log('test');
-          this.show_election_details = false;
-        }
+        url_election_name = params['election_id'];
       }
     );
+
+    this.election_manager.election = { election_id: ls_state.election_id, description: ls_state.description, url_election_name: url_election_name };
+    
   }
 
   election_selected(election_id: number){
--- src/app/manage-election/manage-election.service.ts
+++ src/app/manage-election/manage-election.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { election } from '../shared/election.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ManageElectionService {
+  election: election;
 
   constructor() { }
 }
